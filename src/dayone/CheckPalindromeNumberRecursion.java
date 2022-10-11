4c5b7af59e688622c7c173abf58e73991c203f13
Mamatova Nuraiym <mnurajym9@gmail.com>
2022-10-11 14:52:42 +0600

    day second
    #21DaysCodingChallenge

--- src/dayone/CheckPalindromeNumberRecursion.java
+++ src/dayone/CheckPalindromeNumberRecursion.java
@@ -1,29 +1,30 @@
 package dayone;
-class Solution {
-    public boolean isPalindrome(int x) {
-        int n = x;
 
-        if (isPalindromme(n)) {
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(isPalindromme(in.nextInt()));
+    }
+    public boolean isPalindrome(int x) {
+        int number = x;
+        if (isPalindromme(number)) {
             return true;
         }
         else {
             return false;
         }
-    } public static boolean isPalindromme(int num) {
-        int n = num;
-
-        int rev = 0;
-
-        while (n > 0)
-        {
-            int r = n % 10;
-
-            rev = rev * 10 + r;
-
-            n = n / 10;
+    } public static boolean isPalindromme(int number) {
+        int number1 = number;
+        int number2 = 0;
+        while (number1 > 0) {
+            int r = number1 % 10;
+            number2 = number2 * 10 + r;
+            number1 = number1 / 10;
         }
 
-        return (num == rev);
+        return (number == number2);
     }
 
 
