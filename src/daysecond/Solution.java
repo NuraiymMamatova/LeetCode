7a3b06e3d9ce28ea94a1e87bfc3f7c8d753fd29f
Mamatova Nuraiym <mnurajym9@gmail.com>
2022-10-11 14:58:17 +0600

    day second
    #21DaysCodingChallenge

--- src/daysecond/Solution.java
+++ src/daysecond/Solution.java
@@ -1,7 +1,15 @@
 package daysecond;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Solution {
-        public static int[] twoSum(int[] nums, int target) {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int[] numbers = {in.nextInt(), in.nextInt(), in.nextInt()};
+    System.out.println(Arrays.toString(twoSum(numbers, in.nextInt())));
+    }
+    public static int[] twoSum(int[] nums, int target) {
             for (int i = 0; i < nums.length; i++)
                 for (int j = i + 1; j < nums.length; j++)
                     if (nums[j] == target - nums[i])
